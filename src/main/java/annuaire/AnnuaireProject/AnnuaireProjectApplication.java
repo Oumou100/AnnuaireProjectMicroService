4f05f989c4a046ff8ed1feb7f0811b6671d9a208
Oumou100 <dilymalidi10@gmail.com>
2025-04-07 17:17:29 +0200

    first commit

---
+++ src/main/java/annuaire/AnnuaireProject/AnnuaireProjectApplication.java
@@ -0,0 +1,13 @@
+package annuaire.AnnuaireProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnuaireProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnuaireProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/annuaire/AnnuaireProject/AnnuaireProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package annuaire.AnnuaireProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnuaireProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
